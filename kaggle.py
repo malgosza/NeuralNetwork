dbf9b5491a7b009532205fccbbd16adba78ddf32
Gosia <malgosza.niewiadomska@gmail.com>
2018-11-29 21:30:31 +0100

    wrzucanie do sieci punktow zamiast pliku z punktami

--- kaggle.py
+++ kaggle.py
@@ -4,16 +4,85 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import tensorflow as tf
+import cv2
 
-LEARNING_RATE = 1e-4
+punkty=[
+(153,161),
+(170,138),
+(187,115),
+(204,115),
+(221,115),
+(238,115),
+(255,115),
+(255,138),
+(272,161),
+(272,184),
+(289,184),
+(272,207),
+(289,207),
+(272,230),
+(289,230),
+(272,253),
+(289,253),
+(272,276),
+(272,299),
+(255,299),
+(238,322),
+(221,322),
+(204,345),
+(187,368),
+(170,368),
+(153,391),
+(136,391),
+(119,414),
+(136,414),
+(153,414),
+(170,414),
+(187,414),
+(204,414),
+(221,414),
+(238,414),
+(255,414),
+(272,414),
+(289,414),
+(306,414),
+(323,414),
+(119,437),
+(136,437),
+(153,437),
+(170,437),
+(187,437),
+(204,437),
+(221,437),
+(238,437),
+(255,437),
+(272,437),
+(289,437),
+(306,437),
+(323,437)
+]
+
+# punkty = [(136,230),(272,230),(187,529)]
+img = np.zeros((480,640,1))
+
+for i in range(len(punkty)-1):
+    start = punkty[i]
+    stop = punkty[i + 1]
+    cv2.line(img, start, stop, (255,255,255), 20, 8)
+
+newImage = cv2.resize(img, (28, 28))
+
+obrazekDoSieci=np.array([np.ravel(newImage)])
+
+LEARNING_RATE = 1e-4#jak to zmniejsze i doloze iteracje to uzyskam moj wykres
 # set to 20000 on local environment to get 0.99 accuracy
-TRAINING_ITERATIONS = 2500
+TRAINING_ITERATIONS = 10
 
 DROPOUT = 0.5
-BATCH_SIZE = 50
+BATCH_SIZE = 50#ile probek jest wrzucane do sieci na raz -HIPERARAMETRY
 
 # set to 0 to train on all available data
-VALIDATION_SIZE =2000
+VALIDATION_SIZE =100
 
 # image number to output
 IMAGE_TO_DISPLAY = 10
@@ -289,11 +358,11 @@ if(VALIDATION_SIZE):
     plt.show()
 
 # read test data from CSV file
-test_images = pd.read_csv('asd.csv').values
-test_images = test_images.astype(np.float)
+# test_images = pd.read_csv('test.csv').values
+# test_images = test_images.astype(np.float)
 
 # convert from [0:255] => [0.0:1.0]
-test_images = np.multiply(test_images, 1.0 / 255.0)
+test_images = np.multiply(obrazekDoSieci, 1.0 / 255.0)
 
 # print('test_images({0[0]},{0[1]})'.format(test_images.shape))
 
@@ -313,7 +382,7 @@ print("wynik:" + str(predicted_lables))
 # print('predicted_lables({0})'.format(len(predicted_lables)))
 
 # output test image and prediction
-display(test_images[IMAGE_TO_DISPLAY])
+# display(test_images[IMAGE_TO_DISPLAY])
 # print ('predicted_lables[{0}] => {1}'.format(IMAGE_TO_DISPLAY,predicted_lables[IMAGE_TO_DISPLAY]))
 
 # save results
