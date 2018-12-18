d96c62d34e3fb3647885993b752a8da9f2f22635
Gosia <malgosza.niewiadomska@gmail.com>
2018-12-18 20:40:25 +0100

    jak model jest wytrenowany to dalej nie trenuj

--- kaggle.py
+++ kaggle.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import tensorflow as tf
 import cv2
+import os
 
 punkty=[
 (153,161),
@@ -310,39 +311,42 @@ x_range = []
 
 display_step = 1
 
-for i in range(TRAINING_ITERATIONS):
+if len(os.listdir('modele'))==0:
+    for i in range(TRAINING_ITERATIONS):
 
-    # get new batch
-    batch_xs, batch_ys = next_batch(BATCH_SIZE)
+        # get new batch
+        batch_xs, batch_ys = next_batch(BATCH_SIZE)
 
-    # check progress on every 1st,2nd,...,10th,20th,...,100th... step
-    if i % display_step == 0 or (i + 1) == TRAINING_ITERATIONS:
+        # check progress on every 1st,2nd,...,10th,20th,...,100th... step
+        if i % display_step == 0 or (i + 1) == TRAINING_ITERATIONS:
 
-        train_accuracy = accuracy.eval(feed_dict={x: batch_xs,
-                                                  y_: batch_ys,
-                                                  keep_prob: 1.0})
-        if (VALIDATION_SIZE):
-            validation_accuracy = accuracy.eval(feed_dict={x: validation_images[0:BATCH_SIZE],
-                                                           y_: validation_labels[0:BATCH_SIZE],
-                                                           keep_prob: 1.0})
-            print('training_accuracy / validation_accuracy => %.2f / %.2f for step %d' % (
-            train_accuracy, validation_accuracy, i))
+            train_accuracy = accuracy.eval(feed_dict={x: batch_xs,
+                                                      y_: batch_ys,
+                                                      keep_prob: 1.0})
+            if (VALIDATION_SIZE):
+                validation_accuracy = accuracy.eval(feed_dict={x: validation_images[0:BATCH_SIZE],
+                                                               y_: validation_labels[0:BATCH_SIZE],
+                                                               keep_prob: 1.0})
+                print('training_accuracy / validation_accuracy => %.2f / %.2f for step %d' % (
+                train_accuracy, validation_accuracy, i))
 
-            validation_accuracies.append(validation_accuracy)
+                validation_accuracies.append(validation_accuracy)
 
-        else:
-            print('training_accuracy => %.4f for step %d' % (train_accuracy, i))
-        train_accuracies.append(train_accuracy)
-        x_range.append(i)
+            else:
+                print('training_accuracy => %.4f for step %d' % (train_accuracy, i))
+            train_accuracies.append(train_accuracy)
+            x_range.append(i)
 
-        # increase display_step
-        if i % (display_step * 10) == 0 and i:
-            display_step *= 10
-    # train on batch
-    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, keep_prob: DROPOUT})
+            # increase display_step
+            if i % (display_step * 10) == 0 and i:
+                display_step *= 10
+        # train on batch
+        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, keep_prob: DROPOUT})
 
-saver = tf.train.Saver()
-saver.save(sess,'modele/myModel')
+    saver = tf.train.Saver()
+    saver.save(sess,'modele/myModel')
+
+else:
 
 # check final accuracy on validation set
 if(VALIDATION_SIZE):
